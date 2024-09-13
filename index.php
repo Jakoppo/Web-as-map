0e437d0d97ae2ced6ecfb418954afd430dd1b150
Jakoppo <profiljakub@gmail.com>
2024-09-13 11:28:04 +0200

    yes

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="books-2463779_1280.jpg" alt="Book Collection" usemap="#map1"  >
+    <map id="map1" name="map1">
+        <!-- Podzielono obraz na 16 równych prostokątów -->
+        <!-- Wiersz 1 -->
+        <area shape="rect" coords="0,0,338,239" href="https://allegro.pl/listing?string=dostojewski" alt="dostojewski" title="Dostojewski collection" target="_top">>
+        <area shape="rect" coords="338,0,675,239" href="https://allegro.pl/listing?string=kafka" alt="kafka" title="Kafka collection">
+        <area shape="rect" coords="675,0,1013,239" href="https://allegro.pl/listing?string=dasaj" alt="dasaj" title="Dasaj collection">
+        <area shape="rect" coords="1013,0,1350,239" href="https://booklips.pl/newsy/historyk-teatru-twierdzi-ze-zlokalizowal-dom-w-ktorym-mieszkal-szekspir-w-trakcie-pisania-romea-i-julii/" alt="dostojewski" title="shekspir collection">
+        
+        <!-- Wiersz 2 -->
+        <area shape="rect" coords="0,239,338,478" href="https://allegro.pl/listing?string=dostojewski" alt="dostojewski" title="Dostojewski collection"  target="_top">
+        <area shape="rect" coords="338,239,675,478" href="https://allegro.pl/listing?string=kafka" alt="kafka" title="Kafka collection">
+        <area shape="rect" coords="675,239,1013,478" href="https://allegro.pl/listing?string=dasaj" alt="dasaj" title="Dasaj collection">
+        <area shape="rect" coords="1013,239,1350,478"href="https://booklips.pl/newsy/historyk-teatru-twierdzi-ze-zlokalizowal-dom-w-ktorym-mieszkal-szekspir-w-trakcie-pisania-romea-i-julii/" alt="dostojewski" title="shekspir collection">
+        
+        <!-- Wiersz 3 -->
+        <area shape="rect" coords="0,478,338,717" href="https://allegro.pl/listing?string=dostojewski" alt="dostojewski" title="Dostojewski collection" target="_top">>
+        <area shape="rect" coords="338,478,675,717" href="https://allegro.pl/listing?string=kafka" alt="kafka" title="Kafka collection">
+        <area shape="rect" coords="675,478,1013,717" href="https://allegro.pl/listing?string=dasaj" alt="dasaj" title="Dasaj collection">
+        <area shape="rect" coords="1013,478,1350,717" href="https://booklips.pl/newsy/historyk-teatru-twierdzi-ze-zlokalizowal-dom-w-ktorym-mieszkal-szekspir-w-trakcie-pisania-romea-i-julii/" alt="dostojewski" title="shekspir collection">
+        
+        <!-- Wiersz 4 -->
+        <area shape="rect" coords="0,717,338,955" href="https://allegro.pl/listing?string=dostojewski" alt="dostojewski" title="Dostojewski collection">
+        <area shape="rect" coords="338,717,675,955" href="https://allegro.pl/listing?string=kafka" alt="kafka" title="Kafka collection">
+        <area shape="rect" coords="675,717,1013,955" href="https://allegro.pl/listing?string=dasaj" alt="dasaj" title="Dasaj collection">
+        <area shape="rect" coords="1013,717,1350,955"href="https://booklips.pl/newsy/historyk-teatru-twierdzi-ze-zlokalizowal-dom-w-ktorym-mieszkal-szekspir-w-trakcie-pisania-romea-i-julii/" alt="dostojewski" title="shekspir collection">
+    </map>
+</body>
+</html>
